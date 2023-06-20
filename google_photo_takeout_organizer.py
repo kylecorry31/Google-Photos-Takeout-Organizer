8020279af0333b2f42f7128807f192e54ac1dcdf
Kyle Corry <kylecorry31@gmail.com>
2023-06-20 16:41:08 -0400

    Add progress bar, support for latest takeout format, and smarter duplicate detection

--- google_photo_takeout_organizer.py
+++ google_photo_takeout_organizer.py
@@ -2,18 +2,24 @@ import shutil
 from pathlib import Path
 import json
 import datetime
+from tqdm import tqdm
 
 # Start configuration
-takeout = Path("D:\\Photos\\Takeout\\Google Photos")
-output = Path("D:\\Photos\\Takeout\\Output2")
+takeout = Path("D:\\Photos\\Google Photos")
+output = Path("D:\\Photos\\Organized")
 keep_original_titles = True
 title_format = '%Y-%m-%d %H-%M-%S'
 copy = True
-allow_duplicates = True
+allow_duplicates = False
 # End configuration
 
-photo_metadata = takeout.rglob("*.json")
-count = 0
+photo_metadata = list(takeout.rglob("*.json"))
+ignored_files = [
+    'metadata.json',
+    'print-subscriptions.json',
+    'shared_album_comments.json',
+    'user-generated-memory-titles.json'
+]
 
 def generate_name(date, from_path):
     if keep_original_titles:
@@ -21,19 +27,28 @@ def generate_name(date, from_path):
     extension = from_path.suffix
     return date.strftime(title_format) + extension
 
+def should_rename(from_path, to_path):   
+    if not to_path.exists():
+        return False
+    
+    if allow_duplicates and to_path.exists():
+        return True
+    
+    # Their sizes are different, so they are different files and we should rename
+    return from_path.stat().st_size != to_path.stat().st_size
+
 def move(from_path, to_path):
     to_path.parent.mkdir(parents=True, exist_ok=True)
 
-    if allow_duplicates:
-        # Rename if needed
-        n = 1
-        original = to_path
-        while to_path.exists():
-            parent = original.parent
-            name = original.stem
-            extension = original.suffix
-            to_path = parent / (name + " (" + str(n) + ")" + extension)
-            n += 1
+    # Rename if needed
+    n = 1
+    original = to_path
+    while should_rename(from_path, to_path):
+        parent = original.parent
+        name = original.stem
+        extension = original.suffix
+        to_path = parent / (name + " (" + str(n) + ")" + extension)
+        n += 1
     
     if copy:
         shutil.copy(from_path, to_path)
@@ -54,25 +69,47 @@ def get_image_path(json_path, json):
         path = json_path.parent / name
     return path
 
-for metadata_path in photo_metadata:
-    file = metadata_path.open()
-    contents = file.read()
-    metadata = json.loads(contents)
-    file.close()
-    path = get_image_path(metadata_path, metadata)
-    create_time = int(metadata['photoTakenTime']['timestamp'])
-    time = datetime.datetime.fromtimestamp(create_time)
-    year = time.year
-    month = time.month
-    dest_name = generate_name(time, path)
-    dest = output / str(year) / str(month) / dest_name
-    if not path.exists():
-        print(path, 'not found')
-        continue
-    move(path, dest)
-    count += 1
+with tqdm(total=len(photo_metadata)) as pbar:
+    for metadata_path in photo_metadata:
+        if metadata_path.name in ignored_files:
+            pbar.update(1)
+            continue
 
-if copy:
-    print(str(count), 'photos copied')
-else:
-    print(str(count), 'photos moved')
+        file = metadata_path.open()
+        contents = file.read()
+        metadata = json.loads(contents)
+        file.close()
+        
+        date_keys = ['photoTakenTime', 'creationTime']
+        date_key = None
+        for key in date_keys:
+            if key in metadata:
+                date_key = key
+                break
+
+        if date_key is None:
+            print(metadata_path, 'missing date')
+            pbar.update(1)
+            continue
+
+        is_archived = 'archived' in metadata and metadata['archived']
+        is_trashed = 'trashed' in metadata and metadata['trashed']
+
+        if is_trashed:
+            pbar.update(1)
+            continue
+
+        path = get_image_path(metadata_path, metadata)
+        create_time = int(metadata[date_key]['timestamp'])
+        time = datetime.datetime.fromtimestamp(create_time)
+        year = time.year
+        month = time.month
+        dest_name = generate_name(time, path)
+        dest = output if not is_archived else output / 'Archived'
+        dest = dest / str(year) / str(month) / dest_name
+        if not path.exists():
+            print(path, 'not found')
+            pbar.update(1)
+            continue
+        move(path, dest)
+        pbar.update(1)
