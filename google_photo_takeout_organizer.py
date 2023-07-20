abd3518e20b5a583d6491231aaca5f336f1830e2
Gilmar Pupo <126154+gpupo@users.noreply.github.com>
2023-07-20 16:52:18 -0300

    Add inline functional documentation

--- google_photo_takeout_organizer.py
+++ google_photo_takeout_organizer.py
@@ -5,29 +5,49 @@ import datetime
 from tqdm import tqdm
 
 # Start configuration
-takeout = Path("D:\\Photos\\Google Photos")
-output = Path("D:\\Photos\\Organized")
-keep_original_titles = True
-title_format = '%Y-%m-%d %H-%M-%S'
-copy = True
-allow_duplicates = False
+takeout = Path("D:\\Photos\\Google Photos")  # Path to the directory containing the Google Photos
+output = Path("D:\\Photos\\Organized")  # Path to the output directory for organizing the photos
+keep_original_titles = True  # Whether to keep the original titles of the photos or generate new ones
+title_format = '%Y-%m-%d %H-%M-%S'  # Format for generating new titles
+copy = True  # Whether to copy the files or move them
+allow_duplicates = False  # Whether to allow duplicate files in the output directory
 # End configuration
 
-photo_metadata = list(takeout.rglob("*.json"))
+photo_metadata = list(takeout.rglob("*.json"))  # Get a list of all JSON metadata files
 ignored_files = [
     'metadata.json',
     'print-subscriptions.json',
     'shared_album_comments.json',
     'user-generated-memory-titles.json'
-]
+]  # List of files to ignore during the organization process
 
 def generate_name(date, from_path):
+    """
+    Generate a new name for the photo based on the given date and original file path.
+
+    Args:
+        date (datetime.datetime): The date and time when the photo was taken.
+        from_path (pathlib.Path): The original file path of the photo.
+
+    Returns:
+        str: The new name for the photo.
+    """
     if keep_original_titles:
         return from_path.name
     extension = from_path.suffix
     return date.strftime(title_format) + extension
 
-def should_rename(from_path, to_path):   
+def should_rename(from_path, to_path):
+    """
+    Check if the photo should be renamed based on the from_path and to_path.
+
+    Args:
+        from_path (pathlib.Path): The original file path of the photo.
+        to_path (pathlib.Path): The target file path for the photo.
+
+    Returns:
+        bool: True if the photo should be renamed, False otherwise.
+    """
     if not to_path.exists():
         return False
     
@@ -38,6 +58,13 @@ def should_rename(from_path, to_path):
     return from_path.stat().st_size != to_path.stat().st_size
 
 def move(from_path, to_path):
+    """
+    Move or copy the photo from the source path to the target path.
+
+    Args:
+        from_path (pathlib.Path): The source file path of the photo.
+        to_path (pathlib.Path): The target file path for the photo.
+    """
     to_path.parent.mkdir(parents=True, exist_ok=True)
 
     # Rename if needed
@@ -56,6 +83,16 @@ def move(from_path, to_path):
         shutil.move(from_path, to_path)
 
 def get_image_path(json_path, json):
+    """
+    Get the file path of the image associated with the given JSON metadata.
+
+    Args:
+        json_path (pathlib.Path): The file path of the JSON metadata.
+        json (dict): The parsed JSON metadata.
+
+    Returns:
+        pathlib.Path: The file path of the associated image.
+    """
     stem = json_path.stem
     path = json_path.parent / stem
     if not path.exists():
